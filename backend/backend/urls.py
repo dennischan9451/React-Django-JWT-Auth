bfdd7f39ea21922789ca8080a1151fb0e010f6f1
Daniel Ovieda <ovieda@gmail.com>
2020-11-03 17:42:19 +0700

    install django rest framework & simplejwt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
